f3c678e80b126a6ebf00b6733d493994fe4be60e
mrinnnmoy <mrinmoy202000@gmail.com>
2022-11-13 23:11:03 +0530

    build hero-section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,6 +7,50 @@ const Hero = () => {
     <div className="hero">
       <div className="left-h">
         <Header />
+
+        <div className="the-best-add">
+          <div></div>
+          <span>The best Fitness club in the town.</span>
+        </div>
+
+        {/* Hero heading */}
+        <div className="hero-text">
+          <div>
+            <span className="stroke-text"> Shape </span>
+            <span> Your </span>
+          </div>
+          <div>
+            <span>Ideal Body</span>
+          </div>
+          <div>
+            <span>
+              In here we will help you to shape and build your Ideal
+              body and live our life to fullest.
+            </span>
+          </div>
+        </div>
+
+        {/* Figures */}
+        <div className="figures">
+          <div>
+            <span>+140</span>
+            <span>expert coaches</span>
+          </div>
+          <div>
+            <span>+978</span>
+            <span>members joined</span>
+          </div>
+          <div>
+            <span>+50</span>
+            <span>fitness programs</span>
+          </div>
+        </div>
+
+        {/* Hero Buttons */}
+        <div className="hero-buttons">
+          <button className="btn">Get Started</button>
+          <button className="btn">Learn More</button>
+        </div>
       </div>
       <div className="right-h">Right side</div>
     </div>
