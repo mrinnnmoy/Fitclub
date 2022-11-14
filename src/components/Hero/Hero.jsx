cb9e6e0f435fb6a9df5528c080acccddc73062bf
mrinnnmoy <mrinmoy202000@gmail.com>
2022-11-14 07:06:47 +0530

    build header-right

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import "./Hero.css";
 import Header from "../Header/Header";
+import hero_image from "../../assets/hero_image.png";
+import hero_image_back from "../../assets/hero_image_back.png";
+import Heart from "../../assets/heart.png";
+import Calories from "../../assets/calories.png";
 
 const Hero = () => {
   return (
@@ -52,7 +56,29 @@ const Hero = () => {
           <button className="btn">Learn More</button>
         </div>
       </div>
-      <div className="right-h">Right side</div>
+
+      <div className="right-h">
+        <button className="btn">Join Now</button>
+
+        <div className="heart-rate">
+          <img src={Heart} alt="heart-img" />
+          <span>Heart Rate</span>
+          <span>116 bpm</span>
+        </div>
+
+        {/* Hero images */}
+        <img src={hero_image} alt="" className="hero-image" />
+        <img src={hero_image_back} alt="" className="hero-image-back" />
+
+        {/* Calories */}
+        <div className="calories">
+          <img src={Calories} alt="" />
+          <div>
+            <span>Calories Burned</span>
+            <span>220 kcal</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 };
