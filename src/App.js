8bdbfff8c6928f1c3e0372498058fac16cd64992
mrinnnmoy <mrinmoy202000@gmail.com>
2022-11-16 21:34:20 +0530

    build testimonials-left-secction

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Hero from "./components/Hero/Hero";
 import Programs from "./components/Programs/Programs";
 import Reasons from "./components/Reasons/Reasons";
 import Plans from "./components/Plans/Plans";
+import Testimonials from "./components/Testimonials/Testimonials";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Programs />
       <Reasons />
       <Plans />
+      <Testimonials />
     </div>
   );
 }
