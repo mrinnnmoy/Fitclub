922143fd7483c89aafbf465ba036c202d122f6a6
mrinnnmoy <mrinmoy202000@gmail.com>
2022-11-17 22:45:38 +0530

    build Join-email-function

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import "./Join.css";
+import emailjs from "@emailjs/browser";
 
 const Join = () => {
+    const form = useRef()
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_pxktlvh', 'template_zfx0xfl', form.current, 'DDAYPhlrBahU0jlXa')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+
     return (
         <div className="Join" id="join-us">
             <div className="left-j">
@@ -16,13 +30,15 @@ const Join = () => {
                 </div>
             </div>
             <div className="right-j">
-                <form action="" className="email-container">
-                    <input type="text" className="user_email" placeholder="Enter your Email address." />
+
+                <form ref={form} className="email-container" onSubmit={sendEmail} >
+                    <input type="email" name="user_email" placeholder="Enter your Email address." />
                     <button className="btn btn-join">Join Now</button>
                 </form>
+
             </div>
         </div>
-    )
+    );
 };
 
 export default Join;
