87dbc3ca108958c7e7d81bcc36b49e3b9efa7fb1
mrinnnmoy <mrinmoy202000@gmail.com>
2022-11-18 12:17:23 +0530

    build footer-section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Reasons from "./components/Reasons/Reasons";
 import Plans from "./components/Plans/Plans";
 import Testimonials from "./components/Testimonials/Testimonials";
 import Join from "./components/Join/Join";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
       <Reasons />
       <Plans />
       <Testimonials />
-      <Join />      
+      <Join />
+      <Footer />
     </div>
   );
 }
