705bd355813338e1c5db0ed71adbe52b90f1f565
mrinnnmoy <mrinmoy202000@gmail.com>
2022-11-20 18:26:53 +0530

    build counting-number

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,11 +6,12 @@ import hero_image_back from "../../assets/hero_image_back.png";
 import Heart from "../../assets/heart.png";
 import Calories from "../../assets/calories.png";
 import { motion } from "framer-motion";
+import NumberCounter from "number-counter";
 
 const Hero = () => {
 
   const transition = { type: 'spring', duration: 3 };
-  const mobile = window.innerWidth<=768 ? true: false;
+  const mobile = window.innerWidth <= 768 ? true : false;
 
   return (
     <div className="hero">
@@ -20,7 +21,7 @@ const Hero = () => {
 
         <div className="the-best-add">
           <motion.div
-            initial={{ left: mobile? "165px": "239px" }}
+            initial={{ left: mobile ? "165px" : "239px" }}
             whileInView={{ left: '8px' }}
             transition={{ ...transition, type: 'tween' }}
           ></motion.div>
@@ -47,15 +48,21 @@ const Hero = () => {
         {/* Figures */}
         <div className="figures">
           <div>
-            <span>+140</span>
+            <span>
+              <NumberCounter end={140} start={100} delay='4' prefix="+" />
+            </span>
             <span>expert coaches</span>
           </div>
           <div>
-            <span>+978</span>
+            <span>
+              <NumberCounter end={978} start={900} delay='4' prefix="+" />
+            </span>
             <span>members joined</span>
           </div>
           <div>
-            <span>+50</span>
+            <span>
+              <NumberCounter end={50} start={25} delay='4' prefix="+" />
+            </span>
             <span>fitness programs</span>
           </div>
         </div>
